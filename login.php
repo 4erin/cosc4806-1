45f8342c322866029aeebee52c30f439689f6b2d
4erin <149449116+4erin@users.noreply.github.com>
2025-05-23 05:03:36 +0000

    Added validate page with
    added hardcoded username and password

--- login.php
+++ login.php
@@ -3,12 +3,26 @@
 <html>
  <head>
   <title>login</title>
+   
  </head>
+  
  <body>
 
   <h1>login Form</h1>
-  <p><a href="/login.php">Click here to login</a></p>
+ 
 
+
+
+<form action="/validate.php" method="post">
+  <label for="Username">Username:</label>
+  <br>
+  <input type="text" id="Username" name="username">
+  <br>
+  <label for="password">password:</label><br>
+  <input type="password" id="password" name="password">
+  <br><br>
+  <input type="submit" value="Submit">
+</form>
 
  </body>
 </html>
@@ -16,15 +30,3 @@
 
 
 
-
-
-<form action="/validate.php" method="post">
-  <label for="fname">Username:</label>
-  <br>
-  <input type="text" id="Username" name="username">
-  <br>
-  <label for="lname">password:</label><br>
-  <input type="password" id="password" name="password">
-  <br><br>
-  <input type="submit" value="Submit">
-</form>
