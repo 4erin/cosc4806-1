53581375abf3383af1c6cb2337a8773e5d621636
4erin <149449116+4erin@users.noreply.github.com>
2025-05-23 04:24:11 +0000

    Created login page with form - not functional

---
+++ login.php
@@ -0,0 +1,30 @@
+
+<!DOCTYPE html>
+<html>
+ <head>
+  <title>login</title>
+ </head>
+ <body>
+
+  <h1>login Form</h1>
+  <p><a href="/login.php">Click here to login</a></p>
+
+
+ </body>
+</html>
+
+
+
+
+
+
+<form action="/validate.php" method="post">
+  <label for="fname">Username:</label>
+  <br>
+  <input type="text" id="Username" name="username">
+  <br>
+  <label for="lname">password:</label><br>
+  <input type="password" id="password" name="password">
+  <br><br>
+  <input type="submit" value="Submit">
+</form>
